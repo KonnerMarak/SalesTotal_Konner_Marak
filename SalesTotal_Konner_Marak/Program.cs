982a93c5dd8ffaf56d516983211d9d1979e115d4
Marak <konnermarak@ou.edu>
2020-02-02 15:20:14 -0600

    Add project files.

---
+++ SalesTotal_Konner_Marak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesTotal_Konner_Marak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
