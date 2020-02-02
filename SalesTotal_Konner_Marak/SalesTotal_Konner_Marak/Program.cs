842c76b504f7be92f7c3f5be6c557a1a2d6e330d
Konner Marak <konnermarak@ou.edu>
2020-02-02 16:11:56 -0600

    Completed Sale total which calculates the total, subtotal and sales tax for a purchase ased on user imputs

---
+++ SalesTotal_Konner_Marak/SalesTotal_Konner_Marak/Program.cs
@@ -0,0 +1,43 @@
+﻿// Konner Marak 113423502
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTotal_Konner_Marak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double taxRate = 0.085;
+            Console.WriteLine("What is the name of the product you are purchasing?");
+           string product = Console.ReadLine();
+
+            Console.WriteLine("How many " + product + " do you want to buy?");
+               int productAmount = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("What is the price for each " + product + "?");
+            double price = Convert.ToDouble(Console.ReadLine());
+            double subTotal = price * productAmount;
+            double salesTax = price * productAmount * taxRate;
+            double total = subTotal + salesTax;
+            string subTotalAsstring = subTotal.ToString("C2");
+            string salesTaxAsstring = salesTax.ToString("C2");
+            string totalAsstring = total.ToString("C2");
+
+
+            Console.WriteLine("Your subtotal for your bill is " + subTotalAsstring);
+            Console.WriteLine("Your sales tax for your bill is " + salesTaxAsstring);
+            Console.WriteLine("Your total for your bill is " +totalAsstring );
+
+
+            
+            Console.ReadKey();
+       
+        }
+    }
+}
